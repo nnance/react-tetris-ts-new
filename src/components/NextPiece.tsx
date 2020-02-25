f2a156af3f287d6f56e874fd61bab8ac9e8248f1
Nick Nance <nance.nick@gmail.com>
2020-02-25 07:15:13 -0800

    working piece viewer

--- src/components/NextPiece.tsx
+++ src/components/NextPiece.tsx
@@ -1,31 +1,10 @@
 import React from "react";
-import { Piece, drawBoard, BlockState } from "./drawing";
+import { Piece } from "./drawing";
+import PieceGrid from "./PieceGrid";
 
 type NextPieceProps = { piece: Piece };
 
-const Block = {
-  width: "20px",
-  height: "20px",
-  border: "1px solid black",
-  fontSize: "0.75rem"
-};
-
-const PieceBlock = {
-  ...Block,
-  backgroundColor: "blue"
-};
-
-const EmptyBlock = {
-  ...Block,
-  backgroundColor: "white"
-};
-
-const updateBoard = drawBoard(5, 5);
-
 const NextPiece: React.FC<NextPieceProps> = props => {
-  const drawer = props.piece[0];
-  const state = updateBoard(drawer(1, 0, BlockState.on));
-
   return (
     <div className="col-md-4 col-4" style={{ textAlign: "left" }}>
       <div className="d-none d-md-block">
@@ -33,22 +12,7 @@ const NextPiece: React.FC<NextPieceProps> = props => {
         <br />
       </div>
       <br />
-      <table>
-        <tbody>
-          {state.map((row, rowIdx) => (
-            <tr key={rowIdx}>
-              {row.map((col, idx) => (
-                <td
-                  key={`${rowIdx}, ${idx}`}
-                  style={col ? PieceBlock : EmptyBlock}
-                >
-                  {" "}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <PieceGrid drawer={props.piece[0]} />
     </div>
   );
 };
--- src/components/PieceViewer.tsx
+++ src/components/PieceViewer.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Title from "./Title";
-import NextPiece from "./NextPiece";
-import { drawers } from "./blocks/SBlock";
+import PieceGrid from "./PieceGrid";
+import { drawers as IBlock } from "./blocks/IBlock";
+import { drawers as JBlock } from "./blocks/JBlock";
+import { drawers as LBlock } from "./blocks/LBlock";
+import { drawers as OBlock } from "./blocks/OBlock";
+import { drawers as SBlock } from "./blocks/SBlock";
+import { drawers as TBlock } from "./blocks/TBlock";
+import { drawers as ZBlock } from "./blocks/ZBlock";
+import { Piece } from "./drawing";
 
 type PieceViewerProps = {
   theme: React.CSSProperties;
 };
+
+type BlockTupil = [string, Piece];
+
+const blocks: BlockTupil[] = [
+  ["IBlock", IBlock],
+  ["JBlock", JBlock],
+  ["LBlock", LBlock],
+  ["OBlock", OBlock],
+  ["SBlock", SBlock],
+  ["TBlock", TBlock],
+  ["ZBlock", ZBlock]
+];
 
 const PieceViewer: React.FC<PieceViewerProps> = props => {
   const style: React.CSSProperties = {
@@ -19,9 +38,20 @@ const PieceViewer: React.FC<PieceViewerProps> = props => {
   return (
     <div style={style} className="container-fluid">
       <Title />
-      <div className="row">
-        <NextPiece piece={drawers} />
-      </div>
+      <table>
+        <tbody>
+          {blocks.map(([name, drawers], idx) => (
+            <tr key={idx}>
+              <td style={{ width: "100px", textAlign: "left" }}>{name}</td>
+              {drawers.map((drawer, idx) => (
+                <td key={idx}>
+                  <PieceGrid drawer={drawer} />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
