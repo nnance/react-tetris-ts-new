17e91be416081e46d61eb85639b851dc9529299a
Nick Nance <nance.nick@gmail.com>
2020-02-27 07:02:52 -0800

    extract shared types

---
+++ src/types/game.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import { BoardPiece, Piece } from "../components/drawing";
+
+export type GameState = {
+  piece: BoardPiece;
+  next: Piece;
+  score: number;
+  lineCount: number;
+  level: number;
+  paused: boolean;
+};
+
+export type GameActions = {
+  incrementScore: (value: number) => void;
+  moveDown: () => void;
+  pauseGame: () => void;
+  resumeGame: () => void;
+  startGame: () => void;
+};
+
+export type GameStateSetter = React.Dispatch<React.SetStateAction<GameState>>;
