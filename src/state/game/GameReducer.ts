c69c4814acb065366c66a5e5fe2505e67c2012c5
Nick Nance <nance.nick@gmail.com>
2020-03-09 21:51:18 -0700

    move state transition to game reducer

--- src/state/game/GameReducer.ts
+++ src/state/game/GameReducer.ts
@@ -1,5 +1,18 @@
-import { GameState, GameReducer } from "./types";
-import { stateTransition } from "./StateTransitions";
+import { GameState, GameReducer, GameActions } from "./types";
+import { runningReducer } from "./RunningState";
+import { pausedReducer } from "./PausedState";
 
-export const gameReducer: GameReducer = (state, action): GameState =>
-  stateTransition(state.nextCycle(state, action), action);
+const stateTransition: GameReducer = ({ nextCycle, ...rest }, { type }) => {
+  return type === GameActions.startGame
+    ? { ...rest, nextCycle: runningReducer }
+    : nextCycle === runningReducer && type === GameActions.pauseGame
+    ? { ...rest, nextCycle: pausedReducer }
+    : nextCycle === pausedReducer && type === GameActions.resumeGame
+    ? { ...rest, nextCycle: runningReducer }
+    : { ...rest, nextCycle };
+};
+
+export const gameReducer: GameReducer = (state, action): GameState => {
+  const newState = state.nextCycle(state, action);
+  return stateTransition(newState, action);
+};
