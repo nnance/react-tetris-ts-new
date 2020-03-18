42dad8982007bd999cd33d4a7976b9fada0bce20
Nick Nance <nance.nick@gmail.com>
2020-03-18 07:11:56 -0700

    clean up theme handler

--- src/containers/GameBoard.tsx
+++ src/containers/GameBoard.tsx
@@ -5,12 +5,12 @@ import Header from "../components/Header";
 import StatusSection from "../components/StatusSection";
 import PlayField from "../components/PlayField";
 import NextPiece from "../components/NextPiece";
-import useTheme from "../hooks/useTheme";
 import useGameControls from "../hooks/useGameControls";
 import useFPS from "../hooks/useFPS";
 import { startGame, pauseGame, resumeGame } from "../state/actions";
 import { pausedReducer } from "../state/reducers/PausedState";
 import { startReducer } from "../state/reducers/StartState";
+import ThemeStore from "../state/ThemeStore";
 
 const PlayFieldContainer: React.FC = () => {
   const [game] = React.useContext(GameStore);
@@ -39,27 +39,29 @@ const StatusContainer: React.FC = () => {
 };
 
 export default function GameBoardContainer(): React.ReactElement {
-  const theme = useTheme();
-
   return (
-    <GameProvider>
-      <GameStore.Consumer>
-        {([game, dispatch]): React.ReactElement => (
-          <Container theme={theme}>
-            <Header
-              startHandler={(): void => dispatch(startGame())}
-              pauseHandler={(): void => dispatch(pauseGame())}
-              resumeHandler={(): void => dispatch(resumeGame())}
-              isPaused={game.nextCycle === pausedReducer}
-            />
-            <div className="row">
-              <StatusContainer />
-              <PlayFieldContainer />
-              <NextPiece piece={game.next} />
-            </div>
-          </Container>
-        )}
-      </GameStore.Consumer>
-    </GameProvider>
+    <ThemeStore.Consumer>
+      {([theme]): React.ReactElement => (
+        <GameProvider>
+          <GameStore.Consumer>
+            {([game, dispatch]): React.ReactElement => (
+              <Container theme={theme}>
+                <Header
+                  startHandler={(): void => dispatch(startGame())}
+                  pauseHandler={(): void => dispatch(pauseGame())}
+                  resumeHandler={(): void => dispatch(resumeGame())}
+                  isPaused={game.nextCycle === pausedReducer}
+                />
+                <div className="row">
+                  <StatusContainer />
+                  <PlayFieldContainer />
+                  <NextPiece piece={game.next} />
+                </div>
+              </Container>
+            )}
+          </GameStore.Consumer>
+        </GameProvider>
+      )}
+    </ThemeStore.Consumer>
   );
 }
